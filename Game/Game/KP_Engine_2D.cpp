d1751fa5da668dfe8eb9f39fb3654d6d2e457ec4
Karol Pępiak <115930524+karolpepiak@users.noreply.github.com>
2024-06-24 19:28:22 +0200

    Update
    Day 1
    step 1/34 - creating basic window from scratch

---
+++ Game/Game/KP_Engine_2D.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "Window.h"
+
+int main() 
+{
+	std::cout << "Creating Window\n";
+
+	Window* pWindow = new Window();
+
+	bool running = true;
+
+	while (running)
+	{
+		if (!pWindow->ProcessMessage())
+		{
+			std::cout << "Closing Window\n";
+			running = false;
+		}
+
+		//render
+
+		Sleep(10);
+	}
+
+	delete pWindow;
+
+	return 0;
+}
